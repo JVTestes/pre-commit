7c503baefb76c174340c148aaf5e68ebf644b554
jesus Vieira <jesusvieiradelima@gmail.com>
2015-08-13 10:53:10 -0300

    reparos

--- Composer/Script/Hooks.php
+++ Composer/Script/Hooks.php
@@ -4,20 +4,19 @@ namespace PreCommit\Composer\Script;
 
 use Composer\Script\Event;
 
-$vendorDir = dirname(dirname(__FILE__));
-$baseDir = dirname($vendorDir);
+$fileDir = dirname(dirname(__FILE__));
+$vendorDir = dirname(dirname(dirname(dirname($fileDir))));
 
-
-var_dump($baseDir);die;
-
-define('VENDOR_DIR', __DIR__ . '/../../../../../');
+define('ROOT_DIR', $vendorDir);
 
 class Hooks
 {
     public static function preHooks(Event $event)
     {
         $io = $event->getIO();
-        $gitHook = ROOT_DIR.'.git/hooks/pre-commit';
+        $gitHook = static::config()['git'].
+            DIRECTORY_SEPARATOR.'hooks'.
+            DIRECTORY_SEPARATOR.'pre-commit';
 
         if (file_exists($gitHook)) {
             unlink($gitHook);
@@ -30,10 +29,18 @@ class Hooks
     public static function postHooks(Event $event)
     {
         $io = $event->getIO();
-        $gitHook = ROOT_DIR.'.git/hooks/pre-commit';
-        $docHook = ROOT_DIR.'vendor/juizmill/pre-commit/hooks/pre-commit';
+        $gitHook = static::config()['git'].
+            DIRECTORY_SEPARATOR.'hooks'.
+            DIRECTORY_SEPARATOR.'pre-commit';
+        
+        $docHook = ROOT_DIR.
+            DIRECTORY_SEPARATOR.'vendor'.
+            DIRECTORY_SEPARATOR.'jv-testes'.
+            DIRECTORY_SEPARATOR.'pre-commit'.
+            DIRECTORY_SEPARATOR.'hooks'.
+            DIRECTORY_SEPARATOR.'pre-commit';
 
-        copy($docHook, $gitHook);
+        symlink($docHook, $gitHook);
         chmod($gitHook, 0777);
 
         $io->write('<info>Pre-commit created!</info>');
@@ -41,8 +48,30 @@ class Hooks
         return true;
     }
     
-    private function config()
+    public static function config()
     {
+        $configXml = ROOT_DIR.DIRECTORY_SEPARATOR.'config.xml';
+
+        if (!file_exists($configXml)) {
+            throw new \Exception(sprintf('Configuration file not found!'));
+        }
+
+        $xml = simplexml_load_file($configXml);
+        if (!isset($xml->dir)) {
+            throw new \Exception(sprintf('Configuration node "dir" not found!'));
+        }
+
+        if (!isset($xml->dir->git)) {
+            throw new \Exception(sprintf('Configuration node "git" not found!'));
+        }
+
+        if (!isset($xml->dir->root)) {
+            throw new \Exception(sprintf('Configuration node "root" not found!'));
+        }
         
+        return [
+            'git' => strval($xml->dir->git),
+            'root' => strval($xml->dir->root),
+        ];
     }
 }
